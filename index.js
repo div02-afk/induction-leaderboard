44cb8c48db2bed5e8ce1af7a745e9bfcc7c844ef
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-08-08 19:19:52 +0530

    done

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const { Redis } = require("ioredis");
+const client = new Redis({
+  port: process.env.REDIS_PORT, // Redis port
+  host: process.env.REDIS_HOST, // Redis host
+  password: process.env.REDIS_PASSWORD,
+});
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.post("/", async (req, res) => {
+  const { name, regno, score } = req.body;
+  const user = { name, regno, score };
+  await client.set(regno, JSON.stringify(user));
+  res.status(200).send("Data saved");
+});
+app.get("/", async (req, res) => {
+  const keys = await client.keys("*");
+  console.log(keys);
+  const users = JSON.parse(await client.mget(keys));
+  console.log(users);
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Hello World!');
+  res.status(200).send(users);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
